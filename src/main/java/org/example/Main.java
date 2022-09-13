7bdbe1bfbc5c10aad2543e97290a62120ec8ed58
Sahil Rai <raisahil580@gmail.com>
2022-09-12 19:21:27 -0700

    changed hello world greeting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("My favourite team is Mercedes AMG Petronas Formula 1 Team");
     }
 }
